5ae06302a0ccea9a6a36ff4a6927b0888b73d370
Benedikt Neuhold <benedikt.neuhold@live.at>
2015-03-10 14:10:29 -0500

    Added jCrop into Solution

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -8,10 +8,12 @@ namespace Rimango.ImageField
             var manifest = builder.Add();
 
             manifest.DefineStyle("Rimango.ImageField.Admin").SetUrl("Rimango.ImageField.Admin.css");
+            manifest.DefineStyle("Rimango.ImageField.jCrop").SetUrl("jCrop/jquery.Jcrop.min.css", "jCrop/jquery.Jcrop.css");
 
 
-            manifest.DefineScript("Rimango.ImageField.Base").SetUrl("ImageField.Base.js").SetDependencies("jQuery");
-            manifest.DefineScript("Rimango.ImageField.Crop").SetUrl("ImageField.Crop.js").SetDependencies("jQuery");
+            manifest.DefineScript("Rimango.ImageField.Base").SetUrl("ImageField.Base.js").SetDependencies("jQuery", "Rimango.ImageField.jCrop");
+            manifest.DefineScript("Rimango.ImageField.jCrop").SetUrl("jCrop/jquery.Jcrop.min.js", "jCrop/jquery.Jcrop.js").SetDependencies("jQuery");
+            //manifest.DefineScript("Rimango.ImageField.Crop").SetUrl("ImageField.Crop.js").SetDependencies("jQuery");
         }
     }
 }
